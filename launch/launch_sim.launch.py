3d9b84e2f4d2d08b19ee20caa0aad5288d393148
Arturo <arturo.lobianco@gmail.com>
2023-10-02 18:24:01 +0200

    Aggiunto ROS2_controller

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -36,6 +36,21 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'urobot'],
                         output='screen')
+    
+
+    #dobbiamo spawnare i controller
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
 
 
 
@@ -44,4 +59,6 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
